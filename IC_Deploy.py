7e3926e275ba261ecea8f7577e000f278e88a39e
balemeghana <83078316+balemeghana@users.noreply.github.com>
2022-04-06 13:51:13 +0530

    Add files via upload

---
+++ IC_Deploy.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 03:40:14 2022
+
+@author: PRANAV
+"""
+import streamlit as st
+from tensorflow import keras
+from keras.applications.mobilenet_v2 import MobileNetV2
+import numpy as np
+from PIL import Image
+import cv2
+from keras.applications.mobilenet_v2 import preprocess_input,decode_predictions
+
+st.title('Image Classification using CNN model')
+upload = st.file_uploader('Label = Upload the image')
+model_new = keras.models.load_model("D:\Python_Files\Image_Classification1.h5")
+if upload is not None:
+  file_bytes = np.asarray(bytearray(upload.read()),dtype = np.uint8)
+  opencv_image = cv2.imdecode(file_bytes,1)
+  classes = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+  opencv_image = cv2.cvtColor(opencv_image,cv2.COLOR_BGR2RGB)
+  img = Image.open(upload)
+  st.image(img,caption = 'Uploaded Image',width = 300)
+ # opencv_image/=255.0
+  x = cv2.resize(opencv_image,(180,180), interpolation=cv2.INTER_NEAREST)
+  x = np.expand_dims(x,axis = 0)
+  x = preprocess_input(x)
+  y = model_new.predict(x)
+  #st.write(y)
+  #st.write(f'result: {np.argmax(y)}')
+  st.write('OUTPUT: ')
+  st.write('Result: ',classes[np.argmax(y)])
